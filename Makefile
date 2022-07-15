575b483b13718711dbf3cb466da91d0e952599d5
Merel Van der wal <mevan-de@f1r6s4.codam.nl>
2022-07-15 13:12:54 +0200

    first commit, empty

---
+++ Makefile
@@ -0,0 +1,41 @@
+NAME = push_swap
+CFLAGS ?= -Wall -Wextra -Werror
+CC = gcc
+
+HEADERFILES = include/push_swap.h
+
+SRC_FILES = main.c \
+
+OBJ_FILES = $(SRC_FILES:.c=.o)
+OBJS = $(addprefix obj/, $(OBJ_FILES))
+
+LIBFT = libft/libft.a
+LIBFT_DIR = libft/
+PRINTF = ft_printf/printf.a
+PRINTF_DIR = ft_printf/
+
+all: $(NAME)
+
+$(NAME): $(OBJS) $(HEADERFILES)
+	@make -C $(LIBFT_DIR)
+	@make -C $(PRINTF_DIR)
+	$(CC) $(OBJS) $(LIBFT) $(PRINTF) -o $(NAME)
+
+obj/%.o: src/%.c 
+	@mkdir -p obj
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	rm -f $(OBJS)
+
+norm:
+	norminette libft/ ft_printf/ include/ src/
+
+fclean: clean
+	rm -f $(NAME)
+	@make fclean -C $(LIBFT_DIR)
+	@make fclean -C$(PRINTF_DIR)
+
+re: fclean all
+
+.PHONY: clean fclean re play
---
+++ libft/ft_malloc.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_malloc.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mevan-de <mevan-de@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/05/19 11:56:14 by mevan-de      #+#    #+#                 */
+/*   Updated: 2022/05/19 13:17:25 by mevan-de      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_malloc(size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(size);
+	if (!ptr)
+		exit(0);
+	return (ptr);
+}
