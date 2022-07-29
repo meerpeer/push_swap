f90cb2a550b7cfab2f0164d2146c0e5c76c7f1a0
Merel Van der wal <mevan-de@f1r6s4.codam.nl>
2022-07-29 18:10:31 +0200

    fixed errors from eval

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: merel <merel@student.42.fr>                  +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/15 13:14:33 by mevan-de      #+#    #+#                 */
-/*   Updated: 2022/07/29 15:00:54 by mevan-de      ########   odam.nl         */
+/*   Updated: 2022/07/29 18:09:12 by mevan-de      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,23 +23,14 @@ int	main(int argc, char **argv)
 	t_stack	*stack_a;
 	t_stack	*stack_b;
 	t_moves	moves;
-	char	**split;
 
-	if (is_int(argv[1]))
-	{
-		if (argc <= 1)
-			error_exit();
-		stack_a = parse_input(&argv[1]);
-	}
-	else
-	{
-		split = ft_split(argv[1], ' ');
-		stack_a = parse_input(split);
-	}
+	if (argc <= 1)
+		exit (1);
+	stack_a = parse_input(&argv[1]);
 	stack_b = NULL;
 	moves = init_moves();
 	if (is_sorted(stack_a))
-		return (0);
+		exit (0);
 	try_smallsort(&stack_a, &stack_b);
 	big_sort(&stack_a, &stack_b, &moves);
 	exit (0);
